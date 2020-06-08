36bed58fcc040ee6384162754026db49f89f10b6
sagarduwal <sagarduwal0102@hotmail.com>
2020-06-08 10:06:33 +0545

    :see_no_evil: 🙈 [change] added directory in .gitignore and added testcase
    🙈 [change] added directory in .gitignore and added testcase

--- OrderProcessingService/src/test/java/com/trustgeek/bo/OrderBOImplTest.java
+++ OrderProcessingService/src/test/java/com/trustgeek/bo/OrderBOImplTest.java
@@ -18,16 +18,19 @@ public class OrderBOImplTest {
     @Mock
     OrderDAO orderDAO;
 
+    OrderBOImpl bo;
+
     @Before
     public void setup(){
         MockitoAnnotations.initMocks(this);
+
+        bo = new OrderBOImpl();
+        bo.setOrderDAO(orderDAO);
+
     }
 
     @Test
     public void placeOrder_should_create_an_order() throws SQLException, BOException {
-        OrderBOImpl bo = new OrderBOImpl();
-        bo.setOrderDAO(orderDAO);
-
         Order order = new Order();
         when(orderDAO.create(order)).thenReturn(1);
 
@@ -37,7 +40,82 @@ public class OrderBOImplTest {
     }
 
     @Test
-    public void cancelOrder() {
+    public void placeOrder_should_not_create_an_order() throws SQLException, BOException {
+
+        Order order = new Order();
+        when(orderDAO.create(order)).thenReturn(0);
+
+        boolean result = bo.placeOrder(order);
+        assertFalse(result);
+        verify(orderDAO).create(order);
+    }
+
+    @Test(expected = BOException.class)
+    public void placeOrder_should_throw_BOException() throws SQLException, BOException {
+
+        Order order = new Order();
+        when(orderDAO.create(order)).thenThrow( SQLException.class);
+
+        boolean result = bo.placeOrder(order);
+    }
+
+    @Test
+    public void cancelOrder_should_cancel_order() throws SQLException, BOException {
+        Order order = new Order();
+        order.setId(1);
+        order.setStatus("cancelled");
+
+        when(orderDAO.read(order.getId())).thenReturn(order);
+
+        when(orderDAO.update(order)).thenReturn(1);
+
+        boolean result = bo.cancelOrder(order.getId());
+
+        assertTrue(result);
+        verify(orderDAO).read(order.getId());
+        verify(orderDAO).update(order);
+    }
+
+    @Test
+    public void cancelOrder_should_not_cancel_order() throws SQLException, BOException {
+        Order order = new Order();
+        order.setId(1);
+
+        when(orderDAO.read(order.getId())).thenReturn(order);
+
+        when(orderDAO.update(order)).thenReturn(0);
+
+        boolean result = bo.cancelOrder(order.getId());
+
+        assertFalse(result);
+        verify(orderDAO).read(order.getId());
+        verify(orderDAO).update(order);
+    }
+
+    @Test(expected = BOException.class)
+    public void cancelOrder_should_throw_BOException() throws SQLException, BOException {
+        Order order = new Order();
+        order.setId(1);
+
+        when(orderDAO.read(order.getId())).thenThrow(new SQLException());
+
+//        when(orderDAO.update(order)).thenReturn(0);
+
+        boolean result = bo.cancelOrder(order.getId());
+
+    }
+
+    @Test(expected = BOException.class)
+    public void cancelOrder_should_throw_BOException_on_update() throws SQLException, BOException {
+        Order order = new Order();
+        order.setId(1);
+
+        when(orderDAO.read(order.getId())).thenReturn(order);
+
+        when(orderDAO.update(order)).thenThrow(new SQLException());
+
+        boolean result = bo.cancelOrder(order.getId());
+
     }
 
     @Test
